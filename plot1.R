921be8645614ec3692dbcd3c86a9484aa494f530
ychnjcpu <ychnjcpu@gmail.com>
2018-04-01 20:59:00 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,16 @@
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';',na.strings = '?')
+head(data)
+colnames(data)<-c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+head(data)
+
+#plot 1
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot1.png', width = 480, height = 480, units='px')
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red'))
+
+#close device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,23 @@
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';',na.strings = '?')
+head(data)
+colnames(data)<-c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+head(data)
+
+# Plot 3
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot3.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', cex=1, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+# close device
+dev.off()
